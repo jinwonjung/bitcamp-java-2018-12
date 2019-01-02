fe75b881f889e2adb0705045224784bd714aa51e
jungjinwon <wlsjsl62@naver.com>
2019-01-02 18:47:12 +0900

    업로드

---
+++ java-basic/src/main/java/ch01/Test03.java
@@ -0,0 +1,16 @@
+
+
+package ch01;
+
+public class Test03 {
+/**
+ 이 메서드는 Object 클래스의 메서드를 재정의한 것이다.
+ */
+   @Override
+   public String toString() {
+      return "ok";
+   }
+    public static void main(String[] args) {
+      System.out.println("Hello!");
+   }
+}
